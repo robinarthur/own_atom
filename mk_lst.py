35496303e9fecbbc38857dea1d330b30f14139e4
robinarthur <stukka@gmx.de>
2017-07-20 16:36:03 +0200

    regex

--- mk_lst.py
+++ mk_lst.py
@@ -1,11 +1,18 @@
 #! C:\Users\tank\Anaconda3\python.exe
 
-import os, sys
+import os, sys, re
+
+
+def remove_values_from_list(the_list, val):
+    return [value for value in the_list if value != val]
 
 def main():
     path = "C:/Users/tank/Documents/GitHub/own_atom"
     path2 = "C:/Users/CKr/Documents/GitHub/own_atom"
-    dirs = os.listdir(path2)
+
+    dirs = os.listdir(path)
+    rmv_dirs = re.search('\.',str(dirs))
+    dirs = remove_values_from_list(dirs, rmv_dirs)
 
     f = open("installed_packages.txt", "a")
     print(dirs)
